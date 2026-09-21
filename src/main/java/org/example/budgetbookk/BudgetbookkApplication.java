ba7dfef5c853a2c9c44b5dbf6b52aa9643a6d518
Haipl <katharina.haipl@twinformatics.at>
2026-09-21 09:10:26 +0200

    Initial commit

---
+++ src/main/java/org/example/budgetbookk/BudgetbookkApplication.java
@@ -0,0 +1,13 @@
+package org.example.budgetbookk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetbookkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetbookkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/budgetbookk/BudgetbookkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.budgetbookk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetbookkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
